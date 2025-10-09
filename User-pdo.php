16a58872e365be19b6affc315e1f48c92b87bb3b
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-10-09 16:08:55 +0200

    réalisation de la classe du fichier User-pdo.php

--- User-pdo.php
+++ User-pdo.php
@@ -1,4 +1,23 @@
 <?php
+require_once "User-pdo.php";
 
+class UserPDO{
+    private ?int $id = null;
+    public ?string $login = null;
+    public ?string $email = null;
+    public ?string $login = null;
+    public ?string $firstname = null;
+    public ?string $lastname = null;
+
+
+
+    private PDO $pdo;
+
+    public function __construct(){
+        $this->pdo = get_pdo_connection();
+    }
+
+    public function register()
+}
 
 ?>
