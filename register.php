54187e77b23d7a80895489803e251e21213da7db
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-10-10 16:00:22 +0200

    réalisation formulaire d'inscription

---
+++ register.php
@@ -0,0 +1,17 @@
+<main class ="min-h-screen flex items-center justify-center bg-gray-100">
+    <section class ="bg-white shadow-xl rounded-xl p-8 w-full max-w-md">
+        <h1 class ="text-2xl text-center font-bold mb-6 text-indigo-600">Créer un compte :</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+    </section>
+</main>
